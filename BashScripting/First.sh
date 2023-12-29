0bdcb2e28c920bf9462bb688fc82fbab25e720a9
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-12-29 15:10:36 +0000

    29-12-2023 15:10:33

--- BashScripting/First.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-MY_SHELL="bash"
-echo "I like the $MY_SHELL shell."
-
-echo "I like the ${MY_SHELL} shell."
--- BashScripting/IfElse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MY_SHELL="csh"
-
-if [ "$MY_SHELL" = "bash" ]
-then
-    echo "You seen to like the bash shell"
-else
-    echo "You don't seen to like the bash shell"
-fi
